3ea70b1cfc7e1f3ea9281e9bf44a524a626287a2
Matias Prestes <matiasprestesinformatica@gmail.com>
2025-05-26 05:05:21 +0000

    aa

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,12 +2,13 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 {pkgs}: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
+  channel = "stable-24.05" ; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
     pkgs.zulu
     pkgs.corepack_23
+    pkgs.pnpm_10
   ];
   # Sets environment variables in the workspace
   env = {};
